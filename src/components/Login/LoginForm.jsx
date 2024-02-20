d14d6ca22331e5bdcb5b0b361fe48009acac04ae
jisu <ahteostnqls@naver.com>
2024-02-20 21:42:59 +0900

    feat: 회원가입 여부 관련 state 에 따른 조건부 렌더링 및 toggleState 기능 작업

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -4,6 +4,11 @@ import { InputName, InputStyle, LoginWrap, PageTitle, BtnBlackBg, BtnBlackText }
 export default function LoginForm() {
   const [isJoin, setIsJoin] = useState(false);
 
+  //회원가입 여부 state 토글 기능
+  const toggleState = () => {
+    setIsJoin((state) => !state);
+  };
+
   return (
     <LoginWrap>
       <PageTitle>{isJoin ? "회원가입" : "로그인"}</PageTitle>
@@ -12,13 +17,20 @@ export default function LoginForm() {
         <InputStyle type="text" id="id" />
         <InputName htmlFor="password">비밀번호</InputName>
         <InputStyle type="password" id="password" />
-        <InputName htmlFor="passwordConfirm">비밀번호 확인</InputName>
-        <InputStyle type="password" id="passwordConfirm" />
-        <InputName htmlFor="nickname">닉네임</InputName>
-        <InputStyle type="text" id="nickname" />
+        {isJoin ? (
+          <></>
+        ) : (
+          <>
+            <InputName htmlFor="passwordConfirm">비밀번호 확인</InputName>
+            <InputStyle type="password" id="passwordConfirm" />
+            <InputName htmlFor="nickname">닉네임</InputName>
+            <InputStyle type="text" id="nickname" />
+          </>
+        )}
+
         <div>{isJoin ? <BtnBlackBg>회원가입</BtnBlackBg> : <BtnBlackBg>로그인</BtnBlackBg>}</div>
       </form>
-      <BtnBlackText>{isJoin ? "로그인" : "회원가입"}</BtnBlackText>
+      <BtnBlackText onClick={toggleState}>{isJoin ? "로그인" : "회원가입"}</BtnBlackText>
     </LoginWrap>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,6 @@
-import { Layout1280 } from "components/Layout/LayoutStyles";
 import LoginForm from "components/Login/LoginForm";
 import React from "react";
 
 export default function Login() {
-  return (
-    <Layout1280>
-      <LoginForm />
-    </Layout1280>
-  );
+  return <LoginForm />;
 }
