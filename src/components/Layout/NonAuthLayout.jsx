90a4a93447fb5b5714d67558fd403535304e0400
jisu <ahteostnqls@naver.com>
2024-02-20 21:39:23 +0900

    feat: 미로그인 시 사용되는 NonAuthLayout 컴포넌트에 Outlet 추가

--- src/components/Layout/NonAuthLayout.jsx
+++ src/components/Layout/NonAuthLayout.jsx
@@ -1,5 +1,17 @@
+import Header from "components/Header/Header";
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { Layout1280 } from "./LayoutStyles";
+import Footer from "components/Footer/Footer";
 
 export const NonAuthLayout = () => {
-  return <div>NonAuthLayout</div>;
+  return (
+    <>
+      <Header />
+      <Layout1280>
+        <Outlet />
+      </Layout1280>
+      <Footer />
+    </>
+  );
 };
