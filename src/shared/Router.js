51882ba765b0167c68de9c8e7a7ed781956a3dd9
jisu <ahteostnqls@naver.com>
2024-02-20 12:10:29 +0900

    feat: router 셋팅 - 마이페이지, 로그인

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,8 @@
 import Layout from "components/Layout/Layout";
 import Detail from "pages/Detail";
 import Home from "pages/Home";
+import Login from "pages/Login";
+import MyPage from "pages/MyPage";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -11,6 +13,8 @@ export default function Router() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Layout>
     </BrowserRouter>
