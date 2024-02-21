27b4c4340d37a75468f652722cec47d7ee6f6ab5
jisu <ahteostnqls@naver.com>
2024-02-21 15:51:51 +0900

    feat: 토큰 유무에 따른 라우팅

--- src/components/Layout/AuthLayout.jsx
+++ src/components/Layout/AuthLayout.jsx
@@ -1,10 +1,27 @@
 import Header from "components/Header/Header";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Layout1280 } from "./LayoutStyles";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "components/Footer/Footer";
 
 const AuthLayout = () => {
+  const [isRendered, setIsRendered] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (!accessToken) {
+      navigate("/login");
+    }
+
+    setIsRendered(true);
+  }, []);
+
+  if (!isRendered) {
+    return;
+  }
+
   return (
     <>
       <Header />
--- src/components/Layout/NonAuthLayout.jsx
+++ src/components/Layout/NonAuthLayout.jsx
@@ -1,10 +1,20 @@
 import Header from "components/Header/Header";
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Layout1280 } from "./LayoutStyles";
 import Footer from "components/Footer/Footer";
 
 export const NonAuthLayout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <>
       <Header />
