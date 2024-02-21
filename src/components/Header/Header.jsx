1f69c12b8f2212f5f9eee135d79f62a9090be4cf
jisu <ahteostnqls@naver.com>
2024-02-21 16:12:06 +0900

    feat: cookie에 accessToken 없을 경우 localStorage 유저 데이터 삭제

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { BtnLogout, HeaderContainer } from "./HeaderStyles";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsLogin } from "../../redux/modules/authSlice";
+import { getCookie } from "util/cookie";
 
 export default function Header() {
-  const COOKIE = "accessToken";
+  const COOKIE_KEY = "accessToken";
+  const ACCESS_TOKEN = getCookie(COOKIE_KEY);
   const dispatch = useDispatch();
+  const userId = localStorage.getItem("userId");
   const { isLogin } = useSelector((state) => state.auth);
 
   //로그아웃
   const handleLogout = () => {
-    document.cookie = COOKIE + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = COOKIE_KEY + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     dispatch(setIsLogin());
     localStorage.removeItem("accessToken");
     localStorage.removeItem("userId");
     localStorage.removeItem("avatar");
     localStorage.removeItem("nickname");
   };
+  console.log("ACCESS_TOKEN", ACCESS_TOKEN);
+
+  useEffect(() => {
+    if (!ACCESS_TOKEN) {
+      console.log("토큰 없을 시 실행");
+      if (isLogin) {
+        dispatch(setIsLogin());
+      }
+
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("userId");
+      localStorage.removeItem("avatar");
+      localStorage.removeItem("nickname");
+    }
+  }, [ACCESS_TOKEN]);
 
   return (
     <HeaderContainer>
@@ -25,7 +43,7 @@ export default function Header() {
         <Link to="./">ARTIST</Link>
       </h1>
       <div>
-        {!isLogin ? (
+        {!userId && !ACCESS_TOKEN ? (
           <Link to="/login">로그인</Link>
         ) : (
           <>
