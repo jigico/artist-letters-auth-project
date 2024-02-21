1e70fbb0879c09a13e030b5fa9f1d68eb4eac575
jisu <ahteostnqls@naver.com>
2024-02-21 23:12:16 +0900

    feat: 로그인 쿠키 유효시간 10 -> 60분으로 변경

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -52,9 +52,9 @@ export default function LoginForm() {
   //로그인
   const handleLogin = async (memberObj) => {
     try {
-      const response = await api.post("/login?expiresIn=1m", memberObj, { widthCredentials: true });
+      const response = await api.post("/login", memberObj, { widthCredentials: true });
       const accessToken = response.data.accessToken;
-      setCookie(accessToken, 1);
+      setCookie(accessToken, 60);
       dispatch(setUser(response.data));
       dispatch(setIsLogin());
       navigate("/"); //TODO 여기서 하는거 맞는지 확인 필요
